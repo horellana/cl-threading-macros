cfd5c43d876bf194e116a7d10b85ba92a156f2d9
Hector Orellana <pulga.p.29@gmail.com>
2015-10-08 19:48:04 -0300

    -<> default behavior

--- src/threading-macros.lisp
+++ src/threading-macros.lisp
@@ -1,29 +1,36 @@
 (in-package :cl-user)
 (defpackage threading-macros
   (:use :cl)
-  (:import-from :alexandria
-		:once-only)
   (:export :->)
   (:export :->>)
   (:export :-<>))
 (in-package :threading-macros)
 
+(defun deep-search (thing list)
+  (cond
+    ((null list) nil)
+    ((listp (car list)) (or (deep-search thing (car list))
+			    (deep-search thing (cdr list))))
+    ((equal (car list) thing) t)
+    (t (deep-search thing (cdr list)))))
+
 (defmacro -<> (value &body transformations)
-  (once-only (value)
-    (let ((s (intern "<>")))
-      (if transformations
-	  `(let ((,s ,value))
-	     (-<> ,(car transformations) ,@(cdr transformations)))
-	  value))))
+  (let ((<> (intern "<>")))
+    (if transformations
+	`(let ((,<> ,value))
+	   ,(if (deep-search <> (car transformations))
+		`(-<> ,(car transformations) ,@(cdr transformations))
+		`(-<> (,@(car transformations) ,<>) ,@(cdr transformations))))
+	value)))
 
 (defmacro -> (value &body transformations)
-  (let ((s (intern "<>")))
+  (let ((<> (intern "<>")))
     `(-<> ,value
-       ,@(mapcar #'(lambda (trans) `(,(car trans) ,s ,@(cdr trans)))
+       ,@(mapcar #'(lambda (trans) `(,(car trans) ,<> ,@(cdr trans)))
 		 transformations))))
 
 (defmacro ->> (value &body transformations)
-  (let ((s (intern "<>")))
+  (let ((<> (intern "<>")))
     `(-<> ,value
-       ,@(mapcar #'(lambda (trans) (append trans (list s)))
+       ,@(mapcar #'(lambda (trans) (append trans (list <>)))
 		 transformations))))

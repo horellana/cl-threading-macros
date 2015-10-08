44e96e2b7cbb72918363b019370f199b3163fada
Hector Orellana <pulga.p.29@gmail.com>
2015-10-08 01:02:34 -0300

    modified -<> implementation

--- src/threading-macros.lisp
+++ src/threading-macros.lisp
@@ -8,21 +8,12 @@
   (:export :-<>))
 (in-package :threading-macros)
 
-(defun change-symbol (original new list)
-  (cond
-    ((null list) nil)
-    ((listp (car list)) (cons (change-symbol original new (car list))
-			      (change-symbol original new (cdr list))))
-    (t (cons (if (eq (car list) original)
-		 new
-		 (car list))
-	     (change-symbol original new (cdr list))))))
-
 (defmacro -<> (value &body transformations)
   (once-only (value)
     (let ((s (intern "<>")))
       (if transformations
-	  `(-<> ,(change-symbol s value (car transformations)) ,@(cdr transformations))
+	  `(let ((,s ,value))
+	     (-<> ,(car transformations) ,@(cdr transformations)))
 	  value))))
 
 (defmacro -> (value &body transformations)
